8f45c179554da0db9f858564fab5d43fa5f1fd5e
herr-berna <herr_berna@protonmail.com>
2021-09-23 20:05:08 -0300

    accordion fix, updates

---
+++ assets/js/extra.js
@@ -0,0 +1,5 @@
+let headerToClick = document.getElementById('headingFour4')
+let collapsing = document.getElementById('collapsingFour4')
+headerToClick.addEventListener('click', () => {
+    collapsing.classList.toggle('show')
+})
