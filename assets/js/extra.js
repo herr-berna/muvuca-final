5e1f747b62b1fac21e4b675200b5393e2a2d8ffe
herr-berna <herr_berna@protonmail.com>
2021-09-29 14:32:26 -0300

    updating agenda

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -3,3 +3,15 @@ let collapsing = document.getElementById('collapsingFour4')
 headerToClick.addEventListener('click', () => {
     collapsing.classList.toggle('show')
 })
+
+let headerToClick2 = document.getElementById('headingcs3')
+let collapsing2 = document.getElementById('collapsingcs3')
+headerToClick2.addEventListener('click', () => {
+    collapsing2.classList.toggle('show')
+})
+
+let headerToClick3 = document.getElementById('headinglf3')
+let collapsing3 = document.getElementById('collapsinglf3')
+headerToClick3.addEventListener('click', () => {
+    collapsing3.classList.toggle('show')
+})
