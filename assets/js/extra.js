3e85b877bd422d064fc4e363831c132c115daef2
herr-berna <herr_berna@protonmail.com>
2021-09-29 19:26:30 -0300

    correcting bugs

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -15,3 +15,14 @@ let collapsing3 = document.getElementById('collapsinglf3')
 headerToClick3.addEventListener('click', () => {
     collapsing3.classList.toggle('show')
 })
+
+let headerToClick4 = document.getElementById('headingcm3')
+let collapsing4 = document.getElementById('collapsingcm3')
+headerToClick4.addEventListener('click', () => {
+    collapsing4.classList.toggle('show')
+})
+let headerToClick5 = document.getElementById('headingbt5')
+let collapsing5 = document.getElementById('collapsingbt5')
+headerToClick5.addEventListener('click', () => {
+    collapsing5.classList.toggle('show')
+})
